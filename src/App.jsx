dc3d189330aa40f04803b764249d833bab4a7cf4
imesanza <irantzu.mesanza@gmail.com>
2024-03-11 12:03:09 +0100

    Integración con api in progress

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <div className="container py-5">
         <Filters filterStore={filtersStore} />
-        <Results />
+        {/* <Results /> */}
       </div>
     </>
   );
