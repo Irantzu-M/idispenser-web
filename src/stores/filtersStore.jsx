1c7e1e8c7d5b830cd0961da2483a031dfed2158f
imesanza <irantzu.mesanza@gmail.com>
2024-03-18 07:20:12 +0100

    filtersensor, por que no funciona el try

--- src/stores/filtersStore.jsx
+++ src/stores/filtersStore.jsx
@@ -132,10 +132,12 @@ const useFilterStore = create((set) => ({
     }));
   },
   fetchFilterData: async (parentTab, endpoint) => {
-    console.log("entro el fetchfilterdata");
+    console.log("FILTERSTORE > entro el fetchfilterdata");
     try {
       const response = await fetchApi(endpoint);
       const rawData = await response["items"];
+      console.log("FILTERSTORE > response", response);
+      console.log("FILTERSTORE > rawData", rawData);
 
       if (rawData[0]) {
         set((state) => ({
