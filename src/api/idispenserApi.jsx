5dfdb971e7ac696cefba27c8edc941ad8b7097b2
imesanza <irantzu.mesanza@gmail.com>
2024-03-18 05:51:24 +0100

    siempre saca lista entera

--- src/api/idispenserApi.jsx
+++ src/api/idispenserApi.jsx
@@ -12,7 +12,7 @@ export const fetchApi = async (
   const url = `${API_BASE_URL}`;
   try {
     const response = await Liferay.Util.fetch(url, options);
-    console.log("he entrado en try", endpoint);
+    console.log("he entrado en try", url);
 
     if (!response.ok) {
       throw new Error("Fallo en la conexión");
