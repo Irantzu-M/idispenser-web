fcf05509d19b82802e3967bc34d7135cf290dd3e
imesanza <irantzu.mesanza@gmail.com>
2024-03-19 11:07:37 +0100

    FILTERHUBS

--- src/components/filtersTabContents/_filterHub.jsx
+++ src/components/filtersTabContents/_filterHub.jsx
@@ -1,140 +1,47 @@
-import React from "react";
-import { useEffect, useState } from "react";
-// import SelectedItemsTable from "../filtersSelectableTables/_selectedItemsTable";
+import React, { useEffect, useState } from "react";
 import useFilterStore from "../../stores/filtersStore";
-// import FilterTable from "../filtersTabContents/_filterTable";
 import SearchSectionAutocomplete from "../customSearchSelect/_searchSectionAutocomplete";
 import DefaultTable from "../tables/_defaultTable";
 
-import { getApiData } from "../../api/idispenserApi";
-
 function FilterHubs(props) {
-  const [data, setData] = useState([]);
-  const [remapData, setRemapData] = useState([]);
+  const [data, setData] = useFilterStore((state) => state.selecable) || [];
+  const [remapData, setRemapData] = useState([{}]);
   const fieldsToSearchIn = ["Código HUB", "Cliente ", "Almacén ", "Alias "];
   const fieldsToDisplay = ["Código HUB", "Almacén ", "Alias "];
 
-  // TODO - Use api
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     fetch("http://127.0.0.1:5500/src/mocks/filters/_mockHubs.json")
-  //       .then((response) => response.json())
-  //       .then((rawData) => setData(rawData));
-  //   }
-  //   if (!data[0]) {
-  //     fetchData();
-  //   }
-  //   async function remap() {
-  //     setRemapData(
-  //       data.map((item) => {
-  //         return {};
-  //       })
-  //     );
-  //   }
-  //   remap();
-  // }, [data]);
-
-  // useEffect(() => {
-  //   async function remap() {
-  //     setRemapData(
-  //       data.map((item) => {
-  //         return {
-  //           id: item.id,
-  //           "Código HUB": item.id,
-  //           "Cliente ": item.client,
-  //           "Almacén ": item.storage,
-  //           "Alias ": item.label,
-  //         };
-  //       })
-  //     );
-  //   }
-  //   remap();
-  // }, [data]);
-
-  let endpoint = "hubs/list";
-  useEffect(() => {
-    //console.log("search", searchedText.length, searchedText);
-    if (searchedText.length >= 6) {
-      endpoint += `?search=${searchedText}`;
-      async function fetchData() {
-        try {
-          const response = await getApiData(endpoint);
-          const rawData = await response["items"];
-          if (rawData.length > 0) {
-            setRemapData(remap(rawData));
-          } else {
-            setRemapData([{ "": "no hay resultados" }]);
-          }
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      }
-      if (!data[0]) {
-        fetchData();
-      }
-
-      function remap(dataToremap) {
-        const rmd = dataToremap.map((item) => {
-          return {
-            id: item.idConcentrador,
-            "Código HUB": item.idConcentrador,
-            "Cliente ": item.idCliente,
-            "Almacén ": item.idAlmacenQuirofano,
-            "Alias ": item.aliasIDC,
-          };
-        });
-        return rmd;
-      }
-    }
-  }, [searchedText]);
-
-  // STORE FUNCTIONS
+  // TABS LATERALES
   const tabs = useFilterStore((state) => state.filters);
   const selectedTab = tabs.filter((tab) => {
     if (tab.id == props.filter.id) {
       return tab;
     }
   });
-  const [selectedItems, setSelectedItems] = useState(selectedTab[0].selected); // Estado de elementos seleccionados
 
-  // SEARCH TEXT
+  // TABLA DE ELEMENTOS SELECCIONADOS
+  const [selectedItems, setSelectedItems] = useState(selectedTab[0].selected);
+
+  // TEXTO DE BÚSQUEDA
   const [searchedText, setSearchedText] = useState("");
   const handleChange = (text) => {
     setSearchedText(text);
   };
-  // ITEMS TO DISPLAY IN THE TABLE AFTER SEARCHING
-  const itemsFound = remapData.filter((item) => {
-    const combinedField = fieldsToDisplay
-      .map((field) => {
-        return item[field];
-      })
-      .toString()
-      .replaceAll(",", " ");
-    if (combinedField.toLowerCase().includes(searchedText.toLowerCase())) {
-      return item;
-    }
-  });
 
-  //SELECTABLE para usar tablas default
   const addFilterItem = useFilterStore((state) => state.addFilterItem);
-  const removeFilterItem = useFilterStore((state) => state.removeFilterItem); // Función para eliminar elementos seleccionados
+  const removeFilterItem = useFilterStore((state) => state.removeFilterItem);
   const handleAdd = (itemToAdd) => {
     addFilterItem(itemToAdd, selectedTab[0]);
-    setSelectedItems([...selectedItems, itemToAdd]); // Agregar el elemento seleccionado al estado
+    setSelectedItems([...selectedItems, itemToAdd]);
   };
   const handleRemove = (itemToRemove) => {
-    removeFilterItem(itemToRemove, selectedTab[0]); // Llamar a la función para eliminar elementos seleccionados
-    setSelectedItems(selectedItems.filter((item) => item !== itemToRemove)); // Actualizar el estado de los elementos seleccionados
+    removeFilterItem(itemToRemove, selectedTab[0]);
+    setSelectedItems(selectedItems.filter((item) => item !== itemToRemove));
   };
 
   const handleResetSelection = () => {
-    // Lógica para restablecer los elementos seleccionados
-    // Eliminar todos los elementos seleccionados
     selectedItems.forEach((item) => {
-      removeFilterItem(item, selectedTab[0]); // Llamar a la función para eliminar elementos seleccionados
+      removeFilterItem(item, selectedTab[0]);
+      setSearchedText("");
     });
-
-    // Limpiar la lista de elementos seleccionados
     setSelectedItems([]);
   };
 
@@ -146,27 +53,59 @@ function FilterHubs(props) {
     }
   };
 
+  // LLAMADA A API
+  let endpoint = "hubs/list";
+  const fetchFilterData = useFilterStore((state) => state.fetchFilterData);
+
+  useEffect(() => {
+    if (searchedText.length >= 6) {
+      endpoint = `hubs/list` + `?search=${searchedText}`;
+      try {
+        fetchFilterData(selectedTab, endpoint)
+          .then((data) => {
+            setRemapData(remap(data));
+          })
+          .catch((error) => {
+            console.error("Fallo", error);
+          });
+      } catch (error) {
+        console.error("Fallo al recuperar los datos del HUB");
+      }
+
+      function remap(dataToremap) {
+        const rmd = dataToremap.map((item) => {
+          return {
+            id: item.idConcentrador,
+            "Código HUB": item.idConcentrador,
+            "Almacén ": item.idAlmacenQuirofano,
+            "Alias ": item.aliasIDC,
+          };
+        });
+        return rmd;
+      }
+    }
+  }, [searchedText]);
+
   return (
     <>
-      {remapData[0] && (
-        <div className="mb-3">
-          {selectedItems.length > 0 && (
-            <div className="reset-section">
-              <button onClick={handleResetSelection}>
-                <span className="icon icon-close"></span>Eliminar selección
-              </button>
-            </div>
-          )}
-          <SearchSectionAutocomplete
-            options={remapData}
-            handleChange={handleChange}
-            cellsToDisplay={fieldsToDisplay}
-            cellsToSearchIn={fieldsToSearchIn}
-            placeholder="Search by code or name"
-          />
-        </div>
-      )}
-      {selectedItems.length > 0 && (
+      <div className="mb-3">
+        {selectedItems.length > 0 && (
+          <div className="reset-section">
+            <button onClick={handleResetSelection}>
+              <span className="icon icon-close"></span>Eliminar selección
+            </button>
+          </div>
+        )}
+        <SearchSectionAutocomplete
+          options={remapData}
+          handleChange={handleChange}
+          cellsToDisplay={fieldsToDisplay}
+          cellsToSearchIn={fieldsToSearchIn}
+          placeholder="Search by code or name"
+        />
+      </div>
+
+      {selectedItems[0] && (
         <DefaultTable
           striped
           multiselect
@@ -176,15 +115,21 @@ function FilterHubs(props) {
           customHeader="Artículos seleccionados"
         ></DefaultTable>
       )}
-      {remapData[0] && searchedText.length >= 6 && (
-        <DefaultTable
-          striped
-          multiselect
-          handleSelect={handleSelect}
-          selectedItems={selectedItems}
-          data={itemsFound}
-          className="bg-lighter"
-        ></DefaultTable>
+      {searchedText.length >= 6 && (
+        <>
+          {remapData[0] ? (
+            <DefaultTable
+              striped
+              multiselect
+              handleSelect={handleSelect}
+              selectedItems={selectedItems}
+              data={itemsFound}
+              className="bg-lighter"
+            ></DefaultTable>
+          ) : (
+            <>No hay resultados</>
+          )}
+        </>
       )}
     </>
   );
